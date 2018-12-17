fae9f9387dfba0bf9bd388c3dafc61303d06bd18
chee <chee@snoot.club>
2018-12-17 22:27:47 +0000

    use new serve in index

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 let path = require("path")
-let serve = require("serve-handler")
-let appsRoot = path.resolve(
-	__dirname,
-	"apps"
-)
+
+let appsRoot = path.resolve(__dirname, "apps")
 let pass = require("./library/pass.js")(appsRoot)
 
-let serveOptions = {
-	public: "website",
-	cleanUrls: true,
-	renderSingle: true
-}
+let websiteDirectory = path.resolve(__dirname, "website")
+let serve = require("./library/serve.js")(websiteDirectory)
 
 module.exports = async (request, response) => {
 	return pass(request, response)
 		.catch(error => {
 			if (error.status != 404) {
+				console.log({error})
 				return Promise.reject(error)
 			}
 
-			console.log('no such app, trying files!')
-			return serve(request, response, serveOptions)
+			console.log("no such app! trying files")
+
+			return serve(request, response)
 		})
 }
