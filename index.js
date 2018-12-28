5e6c6b84e95bb0f4a8300106ce14646a413bd426
chee <chee@snoot.club>
2018-12-28 21:46:39 +0000

    use boop for boops now

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-let path = require("path")
+let boop = require("@snootclub/boop")
 
-let appsRoot = path.resolve(__dirname, "apps")
-let pass = require("./library/pass.js")(appsRoot)
-
-let websiteDirectory = path.resolve(__dirname, "website")
-let serve = require("./library/serve.js")(websiteDirectory)
-
-module.exports = async (request, response) => {
-	return pass(request, response)
-		.catch(error => {
-			if (error.status != 404) {
-				console.log({error})
-				return Promise.reject(error)
-			}
-
-			console.log("no such app! trying files")
-
-			return serve(request, response)
-		})
-}
+module.exports = boop
