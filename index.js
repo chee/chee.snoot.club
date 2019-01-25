4293a42ac3d52d979669fbac774a16f33be1256b
chee <chee@snoot.club>
2019-01-25 10:49:26 +0000

    Pull new changes when the rebuild url is hit (#1)
    Pull from github when the rebuild url is hit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 let boop = require("@snootclub/boop")
+let {readFile} = require("fs").promises
+let {exec} = require("child_process")
+let {resolve} = require("path")
 
-module.exports = boop
+let rebuildUrlFilePath = resolve(__dirname, ".rebuild-url")
+
+module.exports = async (request, response) => {
+	let rebuildUrl = await readFile(rebuildUrlFilePath, "utf-8")
+		.catch(() => null)
+
+	if (rebuildUrl && request.url == rebuildUrl) {
+		exec("git pull")
+		return 200
+	}
+
+	return boop(request, response)
+}
