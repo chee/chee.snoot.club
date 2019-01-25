03373dbbd9121eb0420ec17bf68f6692f59aea0d
chee <chee@snoot.club>
2019-01-25 10:40:54 +0000

    Pull from github when the rebuild url is hit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let boop = require("@snootclub/boop")
+let {readFile} = require("fs").promises
+let {exec} = require("child_process")
 
-module.exports = boop
+module.exports = async (request, response) => {
+	let rebuildUrl = await readFile(__dirname + "/.rebuild-url")
+		.catch(() => null)
+
+	if (rebuildUrl && request.url == rebuildUrl) {
+		exec("git pull")
+		return 200
+	}
+
+	return boop(request, response)
+}
