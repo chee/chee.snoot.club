6d2d6b2eb6ddacdc013d9c2c718ceea6b2f5f955
chee <chee@snoot.club>
2019-03-01 23:15:58 +0000

    Remove rebuildurl code

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 let boop = require("@snootclub/boop")
-let {readFile} = require("fs").promises
-let {exec} = require("child_process")
-let {resolve} = require("path")
 
-let rebuildUrlFilePath = resolve(__dirname, ".rebuild-url")
-
-module.exports = async (request, response) => {
-	let rebuildUrl = await readFile(rebuildUrlFilePath, "utf-8")
-		.catch(() => null)
-
-	if (rebuildUrl && request.url == rebuildUrl) {
-		exec("git pull")
-		return 200
-	}
-
-	return boop(request, response)
-}
+module.exports = async (request, response) =>
+	boop(request, response)
