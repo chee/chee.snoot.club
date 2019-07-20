6d001ed3c82a8004848fe2ad97b1d5d203282ac5
chee <chee@snoot.club>
2019-07-20 17:11:03 +0100

    yeet the form through the bus boy

--- boops/telecam/index.js
+++ boops/telecam/index.js
@@ -1,43 +1,68 @@
 let serve = require("serve-handler")
-let cheerio = require("cheerio")
-let fs = require("fs").promises
+let {promises: fs, createWriteStream} = require("fs")
 let resolvePath = require("path").resolve
 let feedFile = resolvePath(__dirname, "website/index.xml")
 let secretFile = resolvePath(__dirname, "secret")
 let photoDirectory = resolvePath(__dirname, "photos")
 let {send} = require("micro")
-let parseBody = require("urlencoded-body-parser")
+let Busboy = require("busboy")
 
-module.exports = async (request, response) => {
+let feedEnd = `
+	</channel>
+</rss>
+`
+
+let createId = () =>
+	Math.random()
+		.toString(36)
+		.slice(2) +
+	Math.random()
+		.toString(36)
+		.slice(2)
+
+module.exports = (request, response) => (async (request, response) => {
 	if (request.url === "/post" && request.method == "POST") {
-		let secret = await fs.readFile(secretFile, "utf-8").catch(() => NaN)
-
-		let body = await parseBody(request)
-
-		if (body.secret.toString() !== secret) {
-			send(response, 444, "yeet")
-		}
-
-		let xmlMode = {xml: {xmlMode: true}}
-
-		let feed = cheerio.load(await fs.readFile(feedFile, "utf-8"), xmlMode)
-
-		let guid =
-			Math.random()
-				.toString(36)
-				.slice(2) +
-			Math.random()
-				.toString(36)
-				.slice(2)
-
-		let title = body.title || guid
-		let date = new Date().toGMTString()
+		let guid = createId()
 		let photoFile = resolvePath(photoDirectory, `${guid}.jpg`)
 
-		await s.writeFile(photoFile, body.photo)
+		let form = await new Promise((succeed, fail) => {
+			let form = {}
+			let boy = new Busboy({
+				headers: request.headers,
+			})
+			boy.on(
+				"file",
+				(field, stream, _originalFilename, _encoding, _mimetype) => {
+					if (field !== "photo") {
+						fail("yeet wrong photo field")
+					}
+					let out = createWriteStream(photoFile)
+					stream.pipe(out)
+				}
+			)
+			boy.on("field", (key, value) => {
+				form[key] = value.toString()
+			})
+			boy.on("finish", () => {
+				succeed(form)
+			})
+			request.pipe(boy)
+		})
 
-		let entry = cheerio.load(
-			`
+
+		let secret = await fs.readFile(secretFile, "utf-8").catch(() => NaN)
+
+		console.log({form, secret})
+		if (form.secret !== secret) {
+			return send(response, 444, "yeet")
+		}
+
+		let feed = await fs.readFile(feedFile, "utf-8")
+
+		let title = form.title || guid
+		let date = new Date().toGMTString()
+
+		let entry = `
 		<item id="${guid}">
 			<title>
 				${title}
@@ -54,23 +79,19 @@ module.exports = async (request, response) => {
 				xmlns:xhtml="http://www.w3.org/1999/xhtml"
 				src="https://chee.snoot.club/telecam/photos/${guid}.jpg"
 			/>
-		</item>`,
-			xmlMode
-		)
+		</item>`
 
 		await fs.writeFile(
 			feedFile,
-			feed("channel")
-				.append(entry)
-				.xml(),
+			feed.replace(feedEnd, entry + feedEnd),
 			"utf-8"
 		)
 
-		return send(response, 200, entry.html())
+		return send(response, 200, entry)
 	}
 
 	return serve(request, response, {
 		public: __dirname + "/website",
 		rewrites: [{source: "/", destination: "/index.xml"}],
 	})
-}
+})(request, response).catch(console.error)
